1fa9466bf4b197719ff383812d9be6902419bbae
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-13 13:20:56 -0400

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <>
+        <a href="#">BadBank</a>
+        <a href="#/CreateAccount/">Create Account</a>
+        <a href="#/login/">Login</a>
+        <a href="#/deposit/">Deposit</a>
+        <a href="#/withdrawal/">Withdrawal</a>
+        <a href="#/balance/">Balance</a>
+        <a href="#/alldata/">All Data</a>
+        </>
+    );
+    }
