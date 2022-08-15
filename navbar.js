66f4161f2f0e86c74e5f66a29ac563f98e1c6973
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-15 18:38:33 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,13 +1,35 @@
 function NavBar(){
     return(
-        <>
-        <a href="#">BadBank</a>
-        <a href="#/CreateAccount/">Create Account</a>
-        <a href="#/login/">Login</a>
-        <a href="#/deposit/">Deposit</a>
-        <a href="#/withdrawal/">Withdrawal</a>
-        <a href="#/balance/">Balance</a>
-        <a href="#/alldata/">All Data</a>
-        </>
-    );
-    }
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">BigBadBank</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#" data-toggle="tooltip" data-placement="top">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/CreateAccount/" data-toggle="tooltip" data-placement="bottom">Create Account</a>
+                </li>
+                <li className="nav-item">
+                     <a className="nav-link" href="#/login/" data-toggle="tooltip" data-placement="top" title="Login to start">Login</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/deposit/" data-toggle="tooltip" data-placement="bottom" title="Save that money">Deposit</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/withdraw/" data-toggle="tooltip" data-placement="top" title="Spend those dollas">Withdraw</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#/alldata/" data-toggle="tooltip" data-placement="top" title="View the family">AllData</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+        
+);
+}
