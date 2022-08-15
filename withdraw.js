8174443a382cc3ab2e1d4ae7fccf9de42a0bb0b2
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-15 19:02:46 -0400

    Create withdraw.js

---
+++ withdraw.js
@@ -0,0 +1,40 @@
+function withdraw(){
+    const ctx = React.useContext(UserContext);
+    let inUser = ctx.loggedIn[0];
+    const [update, setUpdate] = React.useState('false');
+    const [value, setValue] = React.useState('');
+    const [show, setShow] = React.useState(true);
+
+    const handleTextChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    function handleWithdraw(){
+        let balance = document.getElementById('balance').value;
+        if (balance > 0 && inUser.user.balance >= balance && !isNaN(balance)) {
+            inUser.user.balance -= Number(balance);
+            setUpdate(true);
+            setShow(false);
+        }
+        else{
+            alert('Transaction Declined');
+        }
+    }
+
+    return (
+        <Card
+        txtcolor="black"
+        header="Withdraw that cash!"
+        body= {show? (inUser ? (
+            <>
+
+            <h5>{update ? "Balance: " + inUser.user.balance : "Balance: "+ inUser.user.balance}</h5>
+            <h6>Withdraw This Amount</h6>
+            <input type="number" width="300" id="balance" onChange={handleTextChange} value={value}></input>
+            <button type="submit" disabled={ value ?false:true} className="btn btn-light" onClick={handleWithdraw}>Withdraw it now!</button>
+
+            </>
+        ):("Login to see Balance")) : ("Accepted! Balance: $" + inUser.user.balance)}
+       />
+    )
+        }
