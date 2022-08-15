a50dec5423adff3305bdaeafe7b2cf83871fddd2
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-15 19:18:08 -0400

    Create deposit.js

---
+++ deposit.js
@@ -0,0 +1,40 @@
+function deposit(){
+  const ctx = React.useContext(UserContext);
+  let inUser =ctx.loggedIn[0];
+  const [update, setUpdate] = React.useState('false');
+  const [value, setValue] = React.useState('');
+  const [show, setShow] = React.useState(true);
+
+  const handleContextChange = (event) => {
+      setValue(event.target.value);
+  };
+
+  function handleDeposit(){
+      let balance = document.getElementById("balance").value;
+      if (balance > 0 && !isNaN(balance)) {
+      inUser.user.balance += Number(balance);
+      setUpdate(true);
+      setShow(false);
+      }
+      else{
+          alert('Transaction Declined');
+      }
+
+  }
+
+  return (
+      <Card
+      txtcolor="black"
+      Header="Deposit that money!"
+      body = {show? (inUser ? (
+          <>
+          <h4>{update ? "Balance: " + inUser.user.balance : "Balance: "+ inUser.user.balance}</h4>
+          <h5>Deposit this much</h5>
+          <input type="number" width="300" id="balance" onChange={handleContextChange} value={value}></input>
+          <bottom type="submit" disabled={ value ?false:true} className="btn btn-light" onClick={handleDeposit}>Deposit that money!</bottom>
+          
+          </>
+      ):("Login to see balance")) : ("Accepted! Your Balance: $" + inUser.user.balance)}
+     />
+  )
+}
