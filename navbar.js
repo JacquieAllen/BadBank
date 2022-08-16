68c2a2ecca8c0c7b2f0aa0b13c86f8c4e6deedef
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-15 20:22:01 -0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -6,30 +6,26 @@ function NavBar(){
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#" data-toggle="tooltip" data-placement="top">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#/CreateAccount/" data-toggle="tooltip" data-placement="bottom">Create Account</a>
-                </li>
-                <li className="nav-item">
-                     <a className="nav-link" href="#/login/" data-toggle="tooltip" data-placement="top" title="Login to start">Login</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#/deposit/" data-toggle="tooltip" data-placement="bottom" title="Save that money">Deposit</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#/withdraw/" data-toggle="tooltip" data-placement="top" title="Spend those dollas">Withdraw</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#/alldata/" data-toggle="tooltip" data-placement="top" title="View the family">AllData</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#/CreateAccount" data-toggle="tooltip" data-placement="top" title="Create an account with us">CreateAccount<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#/login" data-toggle="tooltip" data-placement="bottom" title="Login to your account">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#/deposit" data-toggle="tooltip" data-placement="top" title="Make a deposit">Deposit</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#/withdraw"data-toggle="tooltip" data-placement="bottom" title="Make a withdraw">Withdraw</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#/AllData"data-toggle="tooltip" data-placement="top" title="View All Data">All Data</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         
 );
 }
