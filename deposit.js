27d0135ff8176fe39dd1b0e8cc8d0e41ae94cf82
Jacquie Allen <100749906+JacquieAllen@users.noreply.github.com>
2022-08-17 00:06:54 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,13 +1,20 @@
 function Deposit(){
   const ctx = React.useContext(UserContext);
   let inUser =ctx.loggedIn[0];
-  const [update, setUpdate] = React.useState('false');
   const [value, setValue] = React.useState('');
+  const [update, setUpdate] = React.useState('false');
   const [show, setShow] = React.useState(true);
 
   const handleContextChange = (event) => {
       setValue(event.target.value);
   };
+
+  const date = new Date(Date.now());
+  const mm = date.getMonth() + 1; 
+  const dd = date.getDate();
+  const year = date.getFullYear();
+  
+  const dateString = `${mm}/${dd}/${year}`;
 
   function handleDeposit(){
       let balance = document.getElementById("balance").value;
@@ -24,17 +31,18 @@ function Deposit(){
 
   return (
       <Card
+      bgcolor="primary"
       txtcolor="black"
       Header="Deposit that money!"
       body = {show? (inUser ? (
           <>
-          <h4>{update ? "Balance: " + inUser.user.balance : "Balance: "+ inUser.user.balance}</h4>
+          <h4>{dateString} {update ? "Balance: " + inUser.user.balance : "Balance: "+ inUser.user.balance}</h4>
           <h5>Deposit this much</h5>
           <input type="number" width="300" id="balance" onChange={handleContextChange} value={value}></input>
           <bottom type="submit" disabled={ value ?false:true} className="btn btn-primary" onClick={handleDeposit}>Deposit that money!</bottom>
           
           </>
-      ): ("")) : ("Accepted! Your Deposit: $" + inUser.user.balance)}
+      ):("")):("Accepted! Your Deposit: $" + inUser.user.balance)}
      />
   )
 }
